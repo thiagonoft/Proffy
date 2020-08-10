10bd7a028fc9ba8d3eb763bbef7df7458e96d8c9
Thiago Almeida <thilemos18@gmail.com>
2020-08-10 09:11:02 -0300

    FrontEnd finalizado

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 import './styles.css';
+
+
 
 function TeacherList() {
     return(
@@ -11,20 +15,36 @@ function TeacherList() {
         <PageHeader title="estes são os proffys disponíveis">
             <form id="search-teachers">
                 
-                <div className="input-block">
-                    <label htmlFor="subject">Matéria</label>
-                    <input type="text" id="subject" />
-                </div>
+            <Select 
+                name="subject" 
+                label="Matéria"
+                options={[
+                    { value: 'Artes', label: 'Artes' },
+                    { value: 'Biologia', label: 'Biologia' },
+                    { value: 'Ciências', label: 'Ciências' },
+                    { value: 'Educação Física', label: 'Educação Física' },
+                    { value: 'Geografia', label: 'Geografia' },
+                    { value: 'História', label: 'História' },
+                    { value: 'Matemática', label: 'Matemática' },
+                    { value: 'Português', label: 'Português' },
+                    { value: 'Química', label: 'Química' },
+                ]}
+            />
+            <Select 
+                name="week_day" 
+                label="Dia da semana"
+                options={[
+                    { value: '0', label: 'Domingo' },
+                    { value: '1', label: 'Segunda' },
+                    { value: '2', label: 'Terça' },
+                    { value: '3', label: 'Quarta' },
+                    { value: '4', label: 'Quinta' },
+                    { value: '5', label: 'Sexta' },
+                    { value: '6', label: 'Sábado' },
+                ]}
+            />
+                <Input type="time" name="time" label="Hora" />
 
-                <div className="input-block">
-                    <label htmlFor="week_day">Dia da semana</label>
-                    <input type="text" id="week_day" />
-                </div>
-
-                <div className="input-block">
-                    <label htmlFor="time">Matéria</label>
-                    <input type="text" id="time" />
-                </div>
 
             </form>
         </PageHeader>
